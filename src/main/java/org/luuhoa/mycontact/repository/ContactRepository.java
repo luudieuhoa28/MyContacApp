f0d81f0222afc42319168f542206f43ab921673c
Hoa <luudieuhoa28@gmail.com>
2020-03-28 18:54:27 +0700

    myContacApp

---
+++ src/main/java/org/luuhoa/mycontact/repository/ContactRepository.java
@@ -0,0 +1,14 @@
+package org.luuhoa.mycontact.repository;
+
+import org.luuhoa.mycontact.entity.Contact;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ContactRepository extends CrudRepository<Contact, Integer> {
+
+    List<Contact> findByNameContaining(String term);
+
+}
---
+++ src/main/java/org/luuhoa/mycontact/service/ContactService.java
@@ -0,0 +1,20 @@
+package org.luuhoa.mycontact.service;
+
+import org.luuhoa.mycontact.entity.Contact;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ContactService {
+
+    Iterable<Contact> findAll();
+
+    List<Contact> search(String term);
+
+    Optional<Contact> findOne(Integer id);
+
+    void save(Contact contact);
+
+    void delete(Integer id);
+
+}
